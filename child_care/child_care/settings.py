27534949b63c09c1bbb06338d15d254914862d58
jonathanmusila <jonathanmusila6@gmail.com>
2019-08-07 11:13:28 +0300

    added secrete key

--- child_care/child_care/settings.py
+++ child_care/child_care/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2s5sls^7jke!4bb6y5-q=fgq^1#(9=5mu(w#h&o$4qkgk&unv2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
